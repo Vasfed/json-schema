ad08f92dd9fb207f32a94c159f39966447a16d29
Vasily Fedoseyev <vasilyfedoseyev@gmail.com>
2011-03-15 17:01:25 +0300

    fix dependencies and valuedependencies in 1.9

--- lib/json-schema/attributes/valuedependencies.rb
+++ lib/json-schema/attributes/valuedependencies.rb
@@ -1,14 +1,15 @@
 module JSON
   class Schema
     class ValueDependenciesAttribute < Attribute
-
       def self.validate(current_schema, data, fragments, validator, options = {})
         if data.is_a?(Hash)
           current_schema.schema['valueDependencies'].each do |property,dependency|
             if data.has_key?(property) && (dependency_value = dependency[data[property]])
-              if dependency_value.is_a?(String) && !data.has_key?(dependency_value)
-                message = "The property '#{build_fragment(fragments)}' has a property '#{property}' value '#{data[property]}' that depends on a missing property '#{dependency_value}'"
-                raise ValidationError.new(message, fragments, current_schema)
+              if dependency_value.is_a?(String)
+                unless data.has_key?(dependency_value)
+                  message = "The property '#{build_fragment(fragments)}' has a property '#{property}' value '#{data[property]}' that depends on a missing property '#{dependency_value}'"
+                  raise ValidationError.new(message, fragments, current_schema)
+                end
               elsif dependency_value.is_a?(Array)
                 dependency_value.each do |value|
                   if !data.has_key?(value)
